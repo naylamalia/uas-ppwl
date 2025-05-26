29511bffc4ef0ff1476df2b9509401567e2ca022
nay <naylanurulamalia@gmail.com>
2025-05-26 18:11:45 +0700

    update user, membuat produk, dan review.

---
+++ database/migrations/2025_05_26_100141_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->enum('category', [
+                'Samsung',
+                'Apple',
+                'Xiaomi',
+                'Vivo',
+                'Oppo',
+                'LG',
+                'Infinix',
+                'Itel',
+                'Realme',
+                'Poco'
+            ]);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('products');
+    }
+};
