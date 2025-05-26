b1d267eff66831e12e510a1f084c980372bbbd65
Tjut Najmi <tjutnajmi@gmail.com>
2025-05-27 01:07:20 +0700

    update route web export pdf dan pindahkan cart controller ke folder customer

---
+++ app/Http/Controllers/Customer/CartController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
+
+class CartController extends Controller
+{
+    // Tampilkan isi keranjang
+    public function index()
+    {
+        $cart = Session::get('cart', []);
+        return view('customer.cart.index', compact('cart'));
+    }
+
+    // Tambah produk ke keranjang
+    public function add(Request $request, $productId)
+    {
+        $product = Product::findOrFail($productId);
+        $cart = Session::get('cart', []);
+
+        // Jika produk sudah ada di keranjang, tambahkan quantity
+        if (isset($cart[$productId])) {
+            $cart[$productId]['quantity'] += 1;
+        } else {
+            $cart[$productId] = [
+                'product_id' => $product->id,
+                'name'       => $product->name,
+                'price'      => $product->price,
+                'quantity'   => 1,
+            ];
+        }
+
+        Session::put('cart', $cart);
+
+        return redirect()->route('cart.index')->with('success', 'Produk berhasil ditambahkan ke keranjang.');
+    }
+
+    // Hapus produk dari keranjang
+    public function remove($productId)
+    {
+        $cart = Session::get('cart', []);
+
+        if (isset($cart[$productId])) {
+            unset($cart[$productId]);
+            Session::put('cart', $cart);
+        }
+
+        return redirect()->route('cart.index')->with('success', 'Produk berhasil dihapus dari keranjang.');
+    }
+}
